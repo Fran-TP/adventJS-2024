baa244d5d961327cf0a4a32b280dad0817c49ffc
Fran-TP <ftorresp@unasam.edu.pe>
2024-12-30 22:58:01 -0500

    Refactor gift set generation to use recursive combinations and sort results by length

--- src/challenge-22/generate-gift-combinatios.ts
+++ src/challenge-22/generate-gift-combinatios.ts
@@ -1,20 +1,21 @@
 function generateGiftSets(gifts: string[]): string[][] {
-  // order is important, so we can't use a set
-
   const result: string[][] = []
-  const len = gifts.length
-  const total = 2 ** len
-  for (let i = 1; i < total; i++) {
-    const binary = i.toString(2).padStart(len, '0')
-    const set: string[] = []
-    for (let j = 0; j < binary.length; j++) {
-      if (binary[j] === '1') {
-        set.push(gifts[j])
-      }
+
+  const generateCombinations = (current: string[], index: number): void => {
+    if (current.length > 0) {
+      result.push([...current])
     }
-    result.push(set)
+
+    for (let i = index; i < gifts.length; i++) {
+      current.push(gifts[i])
+      generateCombinations(current, i + 1)
+      current.pop()
+    }
   }
-  return result
+
+  generateCombinations([], 0)
+
+  return result.sort((a, b) => a.length - b.length)
 }
 
 console.log(generateGiftSets(['car', 'doll', 'puzzle']))
@@ -29,13 +30,13 @@ console.log(generateGiftSets(['car', 'doll', 'puzzle']))
 // ]
 
 console.log(generateGiftSets(['ball']))
-// [
-//   ['ball']
-// ]
+// // [
+// //   ['ball']
+// // ]
 
 console.log(generateGiftSets(['game', 'pc']))
-// [
-//   ['game'],
-//   ['pc'],
-//   ['game', 'pc']
-// ]
+// // [
+// //   ['game'],
+// //   ['pc'],
+// //   ['game', 'pc']
+// // ]
